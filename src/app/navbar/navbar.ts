cca9a54a28e3f0d937dc328f7355782c317d4fef
David <davidb221@gmail.com>
2025-08-14 18:51:45 -0500

    intial navbar function

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
