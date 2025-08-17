68ac1ff10f49ec65d83304e0901502f3f9fa92ce
David <davidb221@gmail.com>
2025-08-17 17:31:58 -0500

    navbar sizing and angular routes added

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,32 @@ import { Component, importProvidersFrom, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
-
+/*
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+*/
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Footer],
+  template: `
+    <app-navbar></app-navbar>
+
+    <main class="main-content">
+      <router-outlet></router-outlet>
+    </main>
+
+    <app-footer></app-footer>
+  `,
+  styleUrl: './app.css'
+})
+
+
 export class App {
   protected readonly title = signal('MetEx');
 }
