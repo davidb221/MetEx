e58817e5333f8c4d2dc0addebae5f76ef824ae1f
David <davidb221@gmail.com>
2025-09-06 20:40:22 -0500

    added about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Faq } from './faq/faq';
 import { Estimate } from './estimate/estimate';
+import { About } from './about/about';
 
 
 export const routes: Routes = [
     { path: "", component: Home,},
     { path: "faq", component: Faq,},
-    { path: "estimate", component: Estimate,}
+    { path: "estimate", component: Estimate,},
+    { path: "about", component: About,}
 ];
