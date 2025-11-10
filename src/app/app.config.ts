b48c33d857520d2536433cbf4218dd4969daf069
David Barboza <davidb221@gmail.com>
2025-11-10 07:55:21 -0600

    scroll to top default behavior for routerLInk

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'})),
-    provideRouter(routes, withInMemoryScrolling({anchorScrolling: "enabled"}))
+    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'}))  
   ]
 };
