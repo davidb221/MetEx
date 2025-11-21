767b8efb3d5264ba839a09db5e4b872ee144fe2b
David Barboza <davidb221@gmail.com>
2025-11-20 22:57:34 -0600

    ng image referencing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import planeImage from '../../assets/plane.png';
+import warehouseImage from '../../assets/warehouse.png';
+import packageDeliveredImage from '../../assets/packageDelivered.png';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,7 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.css'
 })
 export class Home {
-
+  planeImage = planeImage;
+  warehouseImage = warehouseImage;
+  packageDeliveredImage = packageDeliveredImage;
 }
